0984eb18bae85bfb5f9253e65852fa9eef4e11ff
srijith-46 <srijithv.mca22@rvce.edu.in>
2023-10-09 07:21:41 +0530

    LabCycle09

---
+++ REST_API/Product.java
@@ -0,0 +1,24 @@
+package com.rest;
+
+public class Product {
+	
+	String name;
+	int id;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
